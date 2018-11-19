4f7e78a7293b57bb75ba8b214e3cd33945af2950
David Emanuel Sans <davidemanuelsans@gmail.com>
2018-11-19 12:28:33 -0500

    First commit

---
+++ src/main/java/com/david/borderprice/BorderPriceApplication.java
@@ -0,0 +1,12 @@
+package com.david.borderprice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BorderPriceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BorderPriceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/david/borderprice/BorderPriceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.david.borderprice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BorderPriceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
